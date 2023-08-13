7c8f7ca6e937020f1138e6caee734c713016241c
jonathanwang375 <104775998+jonathanwang375@users.noreply.github.com>
2023-08-13 19:45:29 -0400

    Update p5.py
    code to solve pe5

--- project_euler/p5.py
+++ project_euler/p5.py
@@ -1,1 +1,11 @@
 # Smallest Multiple
+
+sum_squares = 0
+sqaure_sum = 0
+sum_natural_num = 0
+
+for index in range(1, 101):
+    sum_natural_num = sum_natural_num + index
+    sum_squares = sum_squares + (index**2)
+square_sum = sum_natural_num**2
+print(square_sum - sum_squares)
