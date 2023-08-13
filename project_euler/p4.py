dabde32c33f90d2f9671db9859bd5818b901e5e9
jonathanwang375 <104775998+jonathanwang375@users.noreply.github.com>
2023-08-13 18:12:13 -0400

    renamed file
    numpalindrome.py become p4.py

--- project_euler/p4.py
+++ project_euler/p4.py
@@ -1,1 +1,12 @@
 # Largest Palindrome Product
+import projecteuler as pe
+
+max_palindrome_num = 0
+test_num = 0
+for a in range(100, 1000):
+    for b in range(100, 1000):
+        test_num = a * b
+        test_palindrome = pe.ispalindrome(test_num)
+        if test_palindrome and test_num > max_palindrome_num:
+            max_palindrome_num = test_num
+print(max_palindrome_num)
