3f4e04550ce0d72d2e79cadc13f2981e63a3a871
jonathanwang375 <104775998+jonathanwang375@users.noreply.github.com>
2023-08-14 15:06:07 -0400

    solved problem 12

--- project_euler/p12.py
+++ project_euler/p12.py
@@ -1,1 +1,13 @@
 # Highly Divisible Triagular Number
+import projecteuler as pe
+
+index = 1
+found = False
+while not found:
+    test_number = pe.triangular_number(index)
+    factor_list = pe.find_factors(test_number)
+    if len(factor_list) >= 500:
+        found = True
+    else:
+        index = index + 1
+print(pe.triangular_number(index))
