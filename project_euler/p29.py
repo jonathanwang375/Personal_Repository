b73979a7fd5e6fe3f3dc9f0329211a8c4ac03a98
jonathanwang375 <104775998+jonathanwang375@users.noreply.github.com>
2023-08-14 16:59:07 -0400

    python format
    formatting code

--- project_euler/p29.py
+++ project_euler/p29.py
@@ -1,11 +1,9 @@
 # Problem 29
-print("Starting Problem 29")
+
 list = []
 for a in range(2, 101):
     for b in range(2, 101):
         test = a**b
         if test not in list:
             list.append(test)
-print("Problem 29")
 print(len(list))
-
