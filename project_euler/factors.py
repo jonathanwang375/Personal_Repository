15801dd39c5bd8c9a87c852c155d095799653960
jonathanwang375 <104775998+jonathanwang375@users.noreply.github.com>
2023-08-14 12:37:35 -0400

    solved pe7

--- project_euler/factors.py
+++ project_euler/factors.py
@@ -1,24 +1,11 @@
 import projecteuler as pe
 
-fact_list_2 = []
-prime_num_list = []
-prime_num_list.append(2)
-prime_num_list.append(3)
-a = 3
-while len(prime_num_list) != 10001:
-    a = a + 2
-    fact_list_2 = find_factors(a)
-    if len(fact_list_2) == 2:
-        prime_num_list.append(a)
-print("The 10001st prime is: " + str(a))
-
-
 fact_list_3 = []
 sum = 5
 current = 3
 while current < 2000000:
     current = current + 2
-    fact_list_3 = find_factors(current)
+    fact_list_3 = pe.find_factors(current)
     if len(fact_list_3) == 2:
         sum = sum + current
 print("The sum is: " + str(sum))
@@ -27,7 +14,7 @@ tri_fact_list = []
 a = 1
 test = 0
 while len(tri_fact_list) < 500:
-    test = triangle(a)
+    test = pe.triangle(a)
     tri_fact_list = find_factors(test)
     a = a + 1
 print(test)
--- project_euler/p7.py
+++ project_euler/p7.py
@@ -1,1 +1,11 @@
 # 10001st Prime
+import projecteuler as pe
+
+number = 3
+count = 2
+while count != 10001:
+    number = number + 2
+    factor_list = pe.find_factors(number)
+    if len(factor_list) == 2:
+        count = count + 1
+print(number)
