99d70203f4875e1934ba77658952cce977daebca
jonathanwang375 <104775998+jonathanwang375@users.noreply.github.com>
2023-08-14 16:00:48 -0400

    solved pe20

--- project_euler/p20.py
+++ project_euler/p20.py
@@ -1,1 +1,9 @@
 # Factorial Digit Sum
+import projecteuler as pe
+
+number = 100
+sum = 0
+number = str(pe.factorial(number))
+for index in range(len(number)):
+    sum = sum + int(number[index])
+print(sum)
