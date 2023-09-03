b1a468950f42d4381bbdfb83aa5229932330a35a
jonathanwang375 <104775998+jonathanwang375@users.noreply.github.com>
2023-09-03 18:06:23 -0400

    Update p89.py
    -need to update to get the correct answer

--- project_euler/p89.py
+++ project_euler/p89.py
@@ -1,7 +1,6 @@
 #Roman Numerals
 import projecteuler as pe
 
-print("Program is starting")
 roman_numeral_list = []
 roman_numeral_value_list = []
 characters_saved = 0
@@ -9,11 +8,11 @@ filename = "p089_roman.txt"
 roman_list = []
 with open(filename, "r") as file:
     roman_list = file.readlines()
-print("This program is running")
 for index in range(len(roman_list)):
-    print(index)
-    test_roman_numeral = roman_list[index]
-    number_convert = pe.convert_roman_to_number(test_roman_numeral)
+    roman_list[index] = roman_list[index].strip()
+for index in range(len(roman_list)):
+    test_word = roman_list[index]
+    number_convert = pe.convert_roman_to_number(test_word)
     string_convert = pe.convert_number_to_roman(number_convert)
-    characters_saved = characters_saved + (len(test_roman_numeral) - len(string_convert))
+    characters_saved = characters_saved + len(test_word) - len(string_convert)
 print(characters_saved)
